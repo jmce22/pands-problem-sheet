1aceb536b653f1a1af1915c90ba3cfd00f961569
jmce22 <108928457+jmce22@users.noreply.github.com>
2023-02-08 00:16:19 +0000

    Update bank.py
    Print command added at end of program. Need to complete program to include euro symbol and floating point.

--- bank.py
+++ bank.py
@@ -9,4 +9,4 @@ amount1 = input('Enter amount 1 in cents: ')
 amount2 = input('Enter amount 2 in cents: ')
 total_cents = int(amount1) + int(amount2)
 total_euro = (total_cents/100)
-print
+print(total_euro)
