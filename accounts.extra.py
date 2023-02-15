e9280f9e56f21d92ff61b45e3fadfef7512bdbe2
James McEneaney <jamesmceneaney17@gmail.com>
2023-02-15 22:09:11 +0000

    code takes input of seven or more digits and hides all but the last four digits

---
+++ accounts.extra.py
@@ -0,0 +1,11 @@
+# This code takes in inputted integer of any size and replaces all digits
+# with the letter 'X', apart from the last four digits.
+# I assume that any number inputted will be at least seven digits long, as there is limited
+# utility to be gained from hiding only one digit from a five digit account or two digits from a six digit account number.
+# An account number of four digits or fewer cannot benefit from a code which hides all but the last four digits.
+# Author: James McEneaney
+
+x = input("please enter a number containing seven or more digits: ")
+intx = int(len(x)-4)
+numberofx = intx*"X"
+print(f"{numberofx}{x[-5:-1]}")
---
+++ accounts.py
@@ -0,0 +1,8 @@
+# this code takes an input of ten digits and outputs
+# the first six digits, followed by the letter 'X' in place of the 
+# last four digits 
+# Author: James McEneaney
+
+x = input("please enter a ten digit number: ")
+modifiedx = f'{"XXXXXX"}{x[-5:-1]}'
+print(modifiedx)
