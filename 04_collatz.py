9c11e741374a5fe6b41d6be0f0a9ba432cfcae4b
James McEneaney <jamesmceneaney17@gmail.com>
2023-04-02 22:25:44 +0100

    this commit is to improve my answer to problem sheet 4; I have added comments, accounted for the case of the user entering a negative integer and experiemented with various potential solutions

--- 04_collatz.py
+++ 04_collatz.py
@@ -6,13 +6,35 @@
 # If the previous term is odd, the next term is 3 times the previous term plus 1. 
 # The conjecture is that for any positive integer chosen, these sequences always reach 1.
 # Author: James McEneaney
+# References: 
 
-n = int(input('Enter a number greater than 1: '))
-print(n)
-while n > 1:
-    if (n % 2) == 0 :
-        n = n//2
-        print(n)
-    else:
-        n = 3*n + 1
-        print(n)
+
+n = int(input("Enter an integer greater than 1: "))
+
+def collatz(n):
+
+# this while loop accounts for the case where the user enters an integer which is negative or zero, and prompts them to enter
+# a positive integer instead
+    while int(n) <= 0:        
+        print("That is not a positive integer")
+        n = int(input("Enter an integer greater than 1: "))
+
+# once the user enters a positive integer of 2 or more, the code runs
+    while n >= 2:
+
+# the second argument, end = " ", causes the numbers to printed in the one line. (Ref: 'A Whirlwind Tour of Python pg 38)
+        print(n, end = " ")   
+
+# this instructs that if a number has no remainder after being divided by two (ie. is an even number),
+# that the number should be divided by 2
+        if n % 2 == 0:        
+            n = n // 2        
+
+ # this instructs that if a number is odd (ie. not even), it should be trebled and have one added to it
+        else:
+            n = n * 3 + 1    
+
+    print(str(1) + " :", end = "\n")
+    print("And so another positive integer succumbs to the gravitational pull of the Collatz conjecture")
+
+print(collatz(n))
